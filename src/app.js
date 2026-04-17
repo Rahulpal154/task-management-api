fa763cf53833c1362c9bb41ec0421c797dd41810
Rahul Pal <rahulpal.moderntechno@gmail.com>
2026-04-17 18:41:27 +0530

    Added root route

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(express.json());
 app.use('/tasks', taskRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Task Management API is running 🚀');
+});
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal server error' });
